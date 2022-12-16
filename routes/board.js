bb1e82e3e5ac47e8f9b2a5695956774e30163d6e
msdou46 <msdou46@gmail.com>
2022-12-16 11:22:34 +0900

    board, comment 예외 처리 완료

--- routes/board.js
+++ routes/board.js
@@ -26,6 +26,10 @@ router.post("/", async (req, res) => {
     const last_board = await Boards.findOne().sort("-boardId").exec() // 있으면 {}, 없으면 null
     let boardId = 0;
 
+    if (!user || !title || !content) {  // 비어있으면 '', 아예 들어오지 않았다면 undefined. 둘 다 false.
+        return res.status(400).json({success: false, message:"데이터 형식이 올바르지 않습니다."})
+    }
+
     if (last_board) {
         boardId = last_board["boardId"] + 1;
     } else {
@@ -33,31 +37,39 @@ router.post("/", async (req, res) => {
     }
 
     const createdBoard = await Boards.create({boardId, user, title, content})   // {} 형태.
-    res.json({message: "성공적으로 등록이 완료되었음"})
+    res.json({success: true, message: "게시글을 등록하였습니다."})
+})
+
+
+
+
+/* -------------------------- 게시글 상세 조회, 수정, 삭제 -------------------------- */
+
+// 미들 웨어. boardId params 를 잘못 입력 받은 경우.
+router.use("/:boardId", (req, res, next) => {
+    const boardId = Number(req.params["boardId"]);   // Number() 를 사용했는데 문자열이 숫자가 아닌 글자다? NaN 반환. 
+    if (Number.isNaN(boardId)) {
+        return res.status(400).send({success: false, message:"데이터 형식이 올바르지 않습니다."});
+    }
+    next();
 })
 
 // 경로 : localhost:3000/boards/:boardId    특정 게시글의 상세 사항을 보여줌. 댓글이 있을 경우 댓글도 함께.
 router.get("/:boardId", async (req, res) => {
-    const boardId = Number(req.params["boardId"]);   // Number() 를 사용했는데 문자열이 숫자가 아닌 글자다? NaN 반환. 
-
-    if (Number.isNaN(boardId)) {
-        return res.status(400).send({success: false, message:"잘못된 ID 입니다"});
-    }
-
+    const {boardId} = req.params;   
     const getBoard = await Boards.findOne({boardId})       // [{}]  findOne() 이라면 {}. 만약 없다면 null
     const getComments = await Comments.find({boardId}); // 만약 없다면 [] 이렇게 빈 배열.
                                                         // 찾는 값이 문자든 숫자든 상관 없어.
     const result = {};
 
     if (getBoard === null || getBoard === undefined) {
-        return res.status(400).json({success: false, message: "존재하지 않는 게시글입니다."})
+        return res.status(404).json({success: false, message: "존재하지 않는 게시글입니다."})
     } else {
         result["board"] = getBoard
     }
     if (getComments.length) {
         result["comments"] = getComments
     }
-
     res.json({success: true, result: result});
 })
 
@@ -67,12 +79,16 @@ router.put('/:boardId', async (req, res) => {
     const {title, content} = req.body;
     const getBoard = await Boards.find({boardId})   
                                 // 있다면 [{}], 없다면 [].      findOne() 은 있다면 {}, 없다면 null
-
-    if (getBoard.length) {
-        await Boards.updateOne({boardId: boardId}, {$set: {title: title, content: content}});
+    if (!title || !content) {
+        return res.status(400).json({success: false, message:"데이터 형식이 올바르지 않습니다."})
     }
 
-    res.status(200).json({success: true})
+    if (!getBoard.length) {
+        return res.status(404).json({success: false, message:"수정 하시려는 게시글은 존재하지 않는 게시글입니다."})
+    } else {
+        await Boards.updateOne({boardId: boardId}, {$set: {title: title, content: content}});
+    }
+    res.status(200).json({success: true, message:"게시글을 수정 하였습니다"})
 })
 
 // 경로 : localhost:3000/boards/:boardId       특정 게시물을 삭제. 게시물에 달린 댓글도 삭제.
@@ -80,12 +96,14 @@ router.delete("/:boardId", async (req, res) => {
     const {boardId} = req.params;
     const existBoard = await Boards.find({boardId})
 
-    if (existBoard.length) {
+    if (!existBoard.length) {
+        return res.status(404).json({success: false, message:"삭제 하시려는 게시글은 존재하지 않는 게시글입니다."})
+    } else {
         await Boards.deleteOne({boardId})
         await Comments.deleteMany({boardId})
     }
 
-    res.status(200).json({success: true})
+    res.status(200).json({success: true, message:"게시글을 삭제 하였습니다"})
 })
 
 
--- routes/comment.js
+++ routes/comment.js
@@ -6,20 +6,26 @@ const Boards = require("../schemas/boards.js")
 const Comments = require("../schemas/comments.js")
 
 
-// 경로 : localhost:3000/comments
-router.get("/", async (req, res) => {
-    res.json({});
+// 미들웨어
+router.use("/:id", async (req, res, next) => {
+    // const method_type = req.method;
+
+    const id = Number(req.params["id"]);   // Number() 를 사용했는데 문자열이 숫자가 아닌 글자다? NaN 반환. 
+    if (Number.isNaN(id)) {
+        return res.status(400).send({success: false, message:"데이터 형식이 올바르지 않습니다."});
+    } 
+    next();
 })
 
 
-// 경로 : localhost:3000/comments/:boardId         해당 게시글에 댓글 달기. 해당 api 는 params 와 body 를 다 받는다.
+// 경로 : localhost:3000/comments/:boardId     해당 게시글에 댓글 달기. 해당 api 는 params 와 body 를 다 받는다.
 router.post("/:boardId", async (req, res) => {
     const {boardId} = req.params;
     const {user, content} = req.body;
     const existBoard = await Boards.find({boardId})
 
     if (!existBoard.length) {
-        return res.status(400).json({success: false, erorrMessage: "해당 게시글은 존재하지 않습니다."})
+        return res.status(404).json({success: false, erorrMessage: "해당 게시글은 존재하지 않습니다."})
     }
 
     const last_comment = await Comments.findOne().sort("-commentId").exec()
@@ -33,33 +39,42 @@ router.post("/:boardId", async (req, res) => {
 
     const createdComment = await Comments.create({commentId, user, content, boardId});     // {}
 
-    res.status(200).json({success: true, comment: createdComment})
+    res.status(200).json({success: true, message:"댓글을 생성하였습니다."})
 })
 
 // 경로 : localhost:3000/comments/:commentId        해당 댓글 수정
 router.put('/:commentId', async (req, res) => {
     const {commentId} = req.params;
     const {content} = req.body;
-    
     const existComment = await Comments.find({commentId})
 
-    if (existComment.length) {
+    if (!content) {
+        return res.status(400).json({success: false, erorrMessage: "수정할 댓글 내용을 입력해 주세요."})
+    }
+
+    if (!existComment.length) {
+        return res.status(404).json({success: false, erorrMessage: "해당 댓글은 존재하지 않습니다."})
+    } else {
         await Comments.updateOne({commentId: commentId}, {$set: {content: content}})
     }
 
-    res.status(200).json({success: true})
+    res.status(200).json({success: true, message:"댓글을 수정하였습니다."})
 })
 
 // 경로 : localhost:3000/comments/:commentId        해당 댓글 삭제
 router.delete("/:commentId", async (req, res) => {
     const {commentId} = req.params;
-    const existComment = await Comments.find({commentId})
+    const existComment = await Comments.find({commentId})   // 있으면 [{}],  없으면 []
 
-    if (existComment.length) {
+    if (!existComment.length) {
+        console.log(existComment);
+        return res.status(404).json({success: false, erorrMessage: "해당 댓글은 존재하지 않습니다."})
+    } else {
+        console.log(existComment);
         await Comments.deleteOne({commentId})
     }
 
-    res.json({success: true})
+    res.json({success: true, message: "댓글을 삭제하였습니다."})
 })
 
 
